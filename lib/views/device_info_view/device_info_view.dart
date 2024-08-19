728aa248f807abe957f47c981ebdf9968aca7b08
Sultan656 <141231023+Sultan656@users.noreply.github.com>
2024-08-19 15:08:06 +0500

    KP-1411, device info ui has been completed

--- lib/views/device_info_view/device_info_view.dart
+++ lib/views/device_info_view/device_info_view.dart
@@ -1,13 +1,18 @@
+import 'dart:convert';
+
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:ip_country_lookup/ip_country_lookup.dart';
 import 'package:ip_country_lookup/models/ip_country_data_model.dart';
 import 'package:network_info_plus/network_info_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:user_info/constants/app_colors.dart';
 import 'package:user_info/ipwhois_service.dart';
 import 'package:user_info/models/ipwhois_model.dart';
 import 'package:user_info/models/otp_response_model.dart';
 import 'package:user_info/models/otp_status_request_model.dart';
+import 'package:user_info/widgets/app_button.dart';
+import 'package:user_info/widgets/app_textfield.dart';
 
 class DeviceInfoScreen extends StatefulWidget {
   const DeviceInfoScreen({super.key});
@@ -19,20 +24,16 @@ class DeviceInfoScreen extends StatefulWidget {
 class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
   Map<String, dynamic>? _deviceData;
   String? _macAddress;
-  String? _ipAddress;
   String? _country;
-  String? _city;
-  String? _region;
-  String? _countryCode;
-  String? _isp;
-
+  IpWhoIsModel? _ipDetails;
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _numberController = TextEditingController();
   String? _otpStatusResponse;
+  String? _errorMessage;
+
   @override
   void initState() {
     super.initState();
-    // Fetch device info after the build is complete
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _initDeviceData();
     });
@@ -43,15 +44,9 @@ class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
     await _fetchDeviceData();
     await _fetchNetworkData();
     await _fetchIpDetails();
-
-
-
   }
 
-
-
   Future<void> _requestPermissions() async {
-    // Request necessary permissions
     if (Theme.of(context).platform == TargetPlatform.android) {
       await [
         Permission.phone,
@@ -82,9 +77,7 @@ class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
         deviceData = {"Error": "Unsupported platform"};
       }
     } catch (e) {
-      deviceData = {
-        "Error": "Failed to get platform version: '${e.toString()}'"
-      };
+      deviceData = {"Error": "Failed to get platform version: '${e.toString()}'"};
     }
 
     if (mounted) {
@@ -94,20 +87,13 @@ class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
     }
   }
 
-  // getDeviceName() async {
-  //   String deviceName = await BluetoothInfo.getDeviceName();
-  //   print('Device Name: $deviceName');
-  // }
-
   Future<void> _fetchNetworkData() async {
     final info = NetworkInfo();
-
     String? macAddress;
     String? ipAddress;
 
     try {
-      macAddress =
-      await info.getWifiBSSID(); // WiFi BSSID often serves as MAC address
+      macAddress = await info.getWifiBSSID(); // WiFi BSSID often serves as MAC address
       ipAddress = await info.getWifiIP();
       if (ipAddress != null) {
         await _fetchCountryAndIsp(ipAddress);
@@ -120,62 +106,36 @@ class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
     if (mounted) {
       setState(() {
         _macAddress = macAddress;
-        // _ipAddress = ipAddress;
       });
     }
   }
 
-
   Future<void> _fetchCountryAndIsp(String ipAddress) async {
     try {
       IpCountryData countryData = await IpCountryLookup().getIpLocationData();
-      final result =  countryData;
-
       if (mounted) {
         setState(() {
-          _country = result.country_name;
-
-          _isp = result.isp;
+          _country = countryData.country_name;
         });
       }
     } catch (e) {
       if (mounted) {
         setState(() {
           _country = "Error fetching country";
-          _isp = "Error fetching ISP";
         });
       }
     }
   }
 
-
   Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
     return {
-//'Board': build.board,
-      //  'Bootloader': build.bootloader,
       'Brand': build.brand,
-      // 'Device': build.device,
       'Display': build.display,
       'Fingerprint': build.fingerprint,
       'Hardware': build.hardware,
-      //  'Host': build.host,
-//      'ID': build.id,
       'Manufacturer': build.manufacturer,
       'Model': build.model,
-      //   'Product': build.product,
-      //    'Tags': build.tags,
-      //   'Type': build.type,
       'Android ID': build.id,
-      //    'Build Fingerprint': build.fingerprint,
-      //     'SDK Int': build.version.sdkInt,
-      //     'Security Patch': build.version.securityPatch,
-      //    'Release': build.version.release,
-      //   'Preview SDK Int': build.version.previewSdkInt,
-      //   'Incremental': build.version.incremental,
-      //    'Is Physical Device': build.isPhysicalDevice,
-      //     'Supported 64-bit ABIs': build.supported64BitAbis.join(', '),
-      //     'Supported 32-bit ABIs': build.supported32BitAbis.join(', '),
-      //     'Supported ABIs': build.supportedAbis.join(', '),
     };
   }
 
@@ -188,253 +148,214 @@ class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
       'Localized Model': data.localizedModel,
       'Identifier for Vendor': data.identifierForVendor,
       'Machine': data.utsname.machine,
-      'Node Name': data.utsname.nodename,
-      'Release': data.utsname.release,
-      'Sysname': data.utsname.sysname,
-      'Version': data.utsname.version,
-      'Is Physical Device': data.isPhysicalDevice,
     };
   }
-  IpWhoIsModel? _ipDetails;
-  bool _loading = false;
-  String? _errorMessage;
-
-  final IpWhoisService _ipWhoisService = IpWhoisService();
 
   _fetchIpDetails() async {
+    final IpWhoisService ipWhoisService = IpWhoisService();
     setState(() {
-      _loading = true;
-      _errorMessage = null;
+      _ipDetails = null; // Reset IP details before fetching new ones
     });
 
     try {
-      final ipDetailsResponse = await _ipWhoisService.fetchIpWhoisDetails();
-      print("Fetched IP details: ${ipDetailsResponse.ip}");
+      final ipDetailsResponse = await ipWhoisService.fetchIpWhoisDetails();
       setState(() {
         _ipDetails = ipDetailsResponse;
       });
-      print("Set state with IP details: ${_ipDetails?.ip}");
     } catch (e) {
       setState(() {
-        _errorMessage = e.toString();
-      });
-    } finally {
-      setState(() {
-        _loading = false;
+        _ipDetails = IpWhoIsModel(ip: 'Error', country: 'Error'); // Use a placeholder on error
       });
     }
   }
 
-  //OtpStatusResponseModel? _otpStatusss;
-  bool _loading2 = false;
-  String? _errorMessage2;
-  _fetchOtpStatus() async {
-    // setState(() {
-    //   _loading2 = true;
-    //   _errorMessage2 = null;
-    // });
-    //
-    // // Ensure _ipDetails is not null before making the request
-    // if (_ipDetails == null) {
-    //   setState(() {
-    //     _errorMessage2 = 'IP details are not loaded yet.';
-    //   });
-    //   return;
-    // }
+  Future<void> _fetchOtpStatus() async {
+    setState(() {
+      _errorMessage = null;
+      _otpStatusResponse = null;
+    });
 
     OtpStatusRequestModel requestModel = OtpStatusRequestModel(
-      iP: _ipDetails?.ip,
-      androidID: _deviceData!['Android ID'] ?? 'Unknown',
-      manufacturer: _deviceData!['Manufacturer'] ?? 'Unknown',
-      modelNo: _deviceData!['Model'] ?? 'Unknown',
-      hardware: _deviceData!['Hardware'] ?? 'Unknown',
-      macAddress: _macAddress!,
-      iEMI: 'cecec', // This should be fetched properly if needed
-      userName: _nameController.text, // This should be fetched properly if needed
-      phoneNo: _numberController.text, // This should be fetched properly if needed
-      geoLocation: _ipDetails?.country ?? 'Unknown',
-      brand: _deviceData!['Brand'] ?? 'Unknown',
-      fingerprint: _deviceData!['Build Fingerprint'] ?? 'Unknown',
+      iP: _ipDetails?.ip ?? 'Unknown',
+      androidID: _deviceData?['Android ID'] ?? 'Unknown',
+      manufacturer: _deviceData?['Manufacturer'] ?? 'Unknown',
+      modelNo: _deviceData?['Model'] ?? 'Unknown',
+      hardware: _deviceData?['Hardware'] ?? 'Unknown',
+      macAddress: _macAddress ?? 'Unknown',
+      iEMI: 'imei', // This should be fetched properly if needed
+      userName: _nameController.text,
+      phoneNo: _numberController.text,
+      geoLocation: _country ?? 'Unknown',
+      brand: _deviceData?['Brand'] ?? 'Unknown',
+      fingerprint: _deviceData?['Fingerprint'] ?? 'Unknown',
     );
-
-
+    print("payload=====${json.encode(requestModel)}");
 
     try {
-      OtpStatusResponseModel response = await _ipWhoisService.otpStatus(requestModel);
-      print("Fetched OTP status: ${response.response}");
+      OtpStatusResponseModel response = await IpWhoisService().otpStatus(requestModel);
       setState(() {
         _otpStatusResponse = response.response;
 
       });
     } catch (e) {
       setState(() {
-        _errorMessage2 = e.toString();
-        print('Error fetching OTP status: $e');
-      });
-    } finally {
-      setState(() {
-        _loading2 = false;
+        _errorMessage = e.toString();
       });
     }
   }
 
+  TableRow _buildTableRow(String title, String value) {
+    return TableRow(
+      children: [
+        Container(
+          color: AppColors.tableBgColor,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              title,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Container(
+          color: AppColors.tableBgColor,
 
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(value),
+          ),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Device Info'),
-      ),
-      body: _deviceData == null &&
-          _macAddress == null &&
-          //_ipAddress == null &&
-          _country == null &&
-          _isp == null
-          ? const Center(child: CircularProgressIndicator())
-          : ListView(
-        padding: const EdgeInsets.all(16.0),
-        children: [
-          if (_deviceData != null)
-            ..._deviceData!.entries.map((entry) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 2,
-                      child: Text(
-                        entry.key,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
+      backgroundColor: AppColors.appBgColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+         //   crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Center(
+                  child: Image.asset('assets/images/app_logo.png', width: 131, height: 33),
+                ),
+              ),
+              const Text("Device Information",style: TextStyle(fontSize: 14.0,fontWeight: FontWeight.w700),),
+              const SizedBox(height: 10.0,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                child: Table(
+                  columnWidths: const {
+                    0: FlexColumnWidth(2),
+                    1: FlexColumnWidth(3),
+                  },
+                  border: TableBorder.all(color: AppColors.textFieldOutline, width: 1),
+                  children: [
+                    if (_deviceData != null)
+                      ..._deviceData!.entries.map(
+                            (entry) => _buildTableRow(entry.key, entry.value.toString()),
                       ),
-                    ),
-                    const SizedBox(width: 10),
-                    Expanded(
-                      flex: 3,
-                      child: Text(
-                        entry.value.toString(),
-                        style: const TextStyle(
-                          color: Colors.blueGrey,
-                        ),
-                      ),
-                    ),
+                    if (_macAddress != null)
+                      _buildTableRow('MAC Address', _macAddress!),
+                    if (_ipDetails != null && _ipDetails!.ip != null)
+                      _buildTableRow('IP Address', _ipDetails!.ip!),
+                    if (_country != null)
+                      _buildTableRow('Country', _country!),
                   ],
                 ),
-              );
-            }).toList(),
-          if (_macAddress != null)
-            _buildInfoTile('MAC Address', _macAddress!),
-
-
-
-          if (_ipDetails?.city != null)
-            _buildInfoTile('City', _ipDetails!.city!),
-          if (_ipDetails?.ip != null)
-            _buildInfoTile('IP', _ipDetails!.ip!),
-          if (_ipDetails?.country != null)
-            _buildInfoTile('Country', _ipDetails!.country!),
-
-          if (_ipDetails?.countryCapital != null)
-            _buildInfoTile('Capital', _ipDetails!.countryCapital!),
-          if (_ipDetails?.region != null)
-            _buildInfoTile('Region', _ipDetails!.region!),
-
-
-
-          ///textfields for name and number
-          // Add text fields for name and number
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: TextField(
-              controller: _nameController,
-              decoration: const InputDecoration(
-                labelText: 'Name',
-                border: OutlineInputBorder(),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: TextField(
-              controller: _numberController,
-              decoration: const InputDecoration(
-                labelText: 'Number',
-                border: OutlineInputBorder(),
+              const SizedBox(height: 16.0,),
+              appTextField(
+                controller: _nameController,
+                title: 'Name',
+                keyboardType: TextInputType.name,
               ),
-              keyboardType: TextInputType.phone,
-            ),
-          ),
+              appTextField(
+                controller: _numberController,
+                title: 'Phone No',
+                keyboardType: TextInputType.phone,
+              ),
+              const SizedBox(height: 22.0,),
+              appButton(
+                title: 'Verify User',
+                onTap: (){
+                  _fetchOtpStatus();
+                  setState(() {
 
-          // Add verify button
-          ElevatedButton(
-            onPressed:(){
-              _fetchOtpStatus();
-              setState(() {
+                  });
+                },
+              ),
+              const SizedBox(height: 10.0,),
+              if (_otpStatusResponse != null && _otpStatusResponse == 'no send otp')
 
-              });
-            } ,
-            child: const Text('Verify'),
-          ),
-
-          // Display the OTP status response
-          if (_otpStatusResponse != null)
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Text(
-                'OTP Status: $_otpStatusResponse',
-                style: const TextStyle(
-                  color: Colors.green,
-                  fontWeight: FontWeight.bold,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 373.0,
+                    height: 60.0 ,
+                   decoration: BoxDecoration(border: Border.all(color: AppColors.appButtonColor),
+                     color: AppColors.appButtonColor.withOpacity(0.60),
+                     borderRadius: BorderRadius.circular(4.0),
+                   ),
+                    child: const Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Returning User',
+                            style:  TextStyle(fontSize: 12,color: Colors.white, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'OTP has not been sent',
+                            style:  TextStyle(fontSize: 12,color: Colors.white, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          if (_errorMessage != null)
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Text(
-                'Error: $_errorMessage',
-                style: const TextStyle(
-                  color: Colors.red,
-                  fontWeight: FontWeight.bold,
+              if(_otpStatusResponse != null && _otpStatusResponse == 'new_user_send_otp')
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 373.0,
+                    height: 60.0 ,
+                    decoration: BoxDecoration(border: Border.all(color: AppColors.redColor),
+                      color: AppColors.redColor.withOpacity(0.50),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    child: const Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'New User',
+                            style:  TextStyle(fontSize: 12,color: Colors.white, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'OTP has been sent',
+                            style:  TextStyle(fontSize: 12,color: Colors.white, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildInfoTile(String title, String value) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Expanded(
-            flex: 2,
-            child: Text(
-              title,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+              if (_errorMessage != null)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Error: $_errorMessage',
+                    style: const TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+                  ),
+                ),
+            ],
           ),
-          const SizedBox(width: 10),
-          Expanded(
-            flex: 3,
-            child: Text(
-              value,
-              style: const TextStyle(
-                color: Colors.blueGrey,
-              ),
-            ),
-          ),
-
-
-        ],
+        ),
       ),
     );
   }
